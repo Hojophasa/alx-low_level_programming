527cefa0572ca72ce676ef84dafd3302d9ae6c46
Hojophasa <asangadocument@gmail.com>
2022-09-09 07:36:59 -0700

    Scripting

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,11 @@
 /**
  * main -is the entry point for the program.
  *
- * Return: zero if no error, Non-zero value if errors.
+ * Return:zero if no error, Non-zero value if errors.
  */
 int main(void)
 {
-	printf("Size of a long int: %Id byte(s)\n", sizeof(char));
+	printf("Size of a long char: %Id byte(s)\n", sizeof(char));
 	printf("Size of an int: %Id byte(s)\n", sizeof(int));
 	printf("Size of a long int: %Id byte(s)\n"' sizeof(long int));
 	printf("Size of a long long int: %Id byte(s)\n", sizeof(long long int));
