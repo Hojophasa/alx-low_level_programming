7fe3a0e94067bd6f753ea364aa73f9b974f83bd7
Hojophasa <asangadocument@gmail.com>
2022-09-09 06:32:27 -0700

    Scripting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+#iclude <stdio.h>
+
 /**
-*main- Serves as the entry point for the program
-*
-*Return: By default returns zero if no erros otherwise returns non-zerro value.
-*/
-int main(voild)
+ * main - printing
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }	
