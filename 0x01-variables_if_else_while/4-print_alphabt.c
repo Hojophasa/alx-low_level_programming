28dcc1a4616d3413c926b2e6277bdcada82b8ff0
Hojophasa <asangadocument@gmail.com>
2022-09-10 07:21:34 -0700

    Scripting

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,15 +13,27 @@
  *     */
 
 int main(void)
-{
-	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (i != 'q' && i != 'e')
-			putchar(i);
-	}
-	putchar('\n');
-	return (0);
+{
+
+		char i;
+
+
+
+			for (i = 'a'; i <= 'z'; i++)
+
+					{
+
+								if (i != 'q' && i != 'e')
+
+												putchar(i);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
 }
+
 
